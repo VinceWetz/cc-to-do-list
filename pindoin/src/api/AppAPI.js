77d18f405965edfb3ef7f5b4fb545240ef89705c
root <root@ip-172-26-4-173.eu-central-1.compute.internal>
2021-02-22 16:00:51 +0000

    added https to api

--- pindoin/src/api/AppAPI.js
+++ pindoin/src/api/AppAPI.js
@@ -6,7 +6,7 @@ export default class AppAPI {
 
     static #api = null;
 
-    #appServerBaseURL = 'http://172.26.4.173:5000';
+    #appServerBaseURL = 'https://18.198.252.55:5000';
 
     // fetch and return list of json objects
 
