33f28385247f804564113e7df4629f694a1e85c6
Duc Nguyen <dn031@hdm-stuttgart.de>
2021-02-24 17:37:48 +0100

    Bugfix Join  Button

--- pindoin/src/index.js
+++ pindoin/src/index.js
@@ -64,20 +64,17 @@ class WelcomePage extends React.Component {
     }
 
     handleJoin = async (e) => {
+
         
             // Prüfen, ob Liste mit ID existiert und in State speichern
+            e.preventDefault();
             const list = await AppAPI.getAPI().getList(this.state.listId)
             if (list.length >= 1) {
                 this.setState({success: true, list: list[0]})
             } else {
                 this.setState({success: false, list: NaN})
             }
-         
-        // const list = await AppAPI.getAPI().getList(this.state.listId)
-        // console.log(list)
-        // this.setState({
-        //     list: list
-        // })
+
     }
     
     renderRedirect = () => {
